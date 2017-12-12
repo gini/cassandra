2682df772effc91dd5926ad22c16a99a92f825c0
David González <godboole@gmail.com>
2017-12-12 10:11:10 +0100

    Remove remnant from OSX sed implementation

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ renderDockerfile () {
   mkdir -p $VERSION
   cp -a Dockerfile.template $VERSION/Dockerfile
   cp -a scripts/entrypoint.sh $VERSION/entrypoint.sh
-  sed -i.bk "s/CASSANDRA_VERSION/$VERSION/g" $VERSION/Dockerfile
+  sed -i "s/CASSANDRA_VERSION/$VERSION/g" $VERSION/Dockerfile
 }
 
 if [ -z $VERSION ] ; then
